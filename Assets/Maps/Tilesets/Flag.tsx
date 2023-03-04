93c8e8afd277b1c35fc822016fc7b92acbeefe5e
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-03-04 17:18:55 +0000

    Add mock up tiles and course

---
+++ Assets/Maps/Tilesets/Flag.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Flag" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="Flag.png" width="128" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
