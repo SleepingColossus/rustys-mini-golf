4d37112b965efb661ad1484fe9f9b6299cda541a
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-03-13 21:01:22 +0000

    Fix file paths in Tiled tile sets

--- Assets/Maps/Tilesets/Tileset.tsx
+++ Assets/Maps/Tilesets/Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="36" columns="6">
- <image source="Tileset.png" width="192" height="192"/>
+ <image source="../../../web/sprites/Tileset.png" width="192" height="192"/>
 </tileset>
