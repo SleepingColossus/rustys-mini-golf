96e0c1bd12bf390842557e8d4142c5ce3ab4666e
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-07-18 08:42:47 +0100

    Add game state enum

--- Src/Maps/Gameplay.gd
+++ Src/Maps/Gameplay.gd
@@ -1,52 +1,63 @@
 extends Node2D
 
+enum State {
+    INTRO_STATE,
+    GAMEPLAY_STATE
+}
+
+var _state : State
 var _mouse_position = null
 var _is_mouse_down := false
 @onready var _ball = $Ball
 
+
+func _ready():
+    _state = State.INTRO_STATE
 
 func _process(_delta):
     if Input.is_action_just_pressed("toggle_zoom"):
         _toggle_zoom()
 
 func _input(event):
-    if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT:
-            if event.is_pressed():
-                _is_mouse_down = true
-            else:
-                _is_mouse_down = false
-                _mouse_released()
+    if _state == State.GAMEPLAY_STATE:
+        if event is InputEventMouseButton:
+            if event.button_index == MOUSE_BUTTON_LEFT:
+                if event.is_pressed():
+                    _is_mouse_down = true
+                else:
+                    _is_mouse_down = false
+                    _mouse_released()
 
-    if event is InputEventMouseMotion and _is_mouse_down:
-        _mouse_position = get_local_mouse_position()
-    else:
-        _mouse_position = null
+        if event is InputEventMouseMotion and _is_mouse_down:
+            _mouse_position = get_local_mouse_position()
+        else:
+            _mouse_position = null
 
-    queue_redraw()
+        queue_redraw()
 
 
 func _draw():
-    if _mouse_position != null and _ball.can_move():
-        var end_point = _mouse_position
-        var distance = _ball.position.distance_to(end_point)
+    if _state == State.GAMEPLAY_STATE:
+        if _mouse_position != null and _ball.can_move():
+            var end_point = _mouse_position
+            var distance = _ball.position.distance_to(end_point)
 
-        if distance > 100:
-            var direction = (end_point - _ball.position).normalized()
-            end_point = _ball.position + direction * 100
+            if distance > 100:
+                var direction = (end_point - _ball.position).normalized()
+                end_point = _ball.position + direction * 100
 
-        var color : Color
+            var color : Color
 
-        if distance < 25:
-            color = Color.YELLOW
-        elif distance < 50:
-            color = Color.CORAL
-        elif distance < 75:
-            color = Color.ORANGE
-        else:
-            color = Color.RED
+            if distance < 25:
+                color = Color.YELLOW
+            elif distance < 50:
+                color = Color.CORAL
+            elif distance < 75:
+                color = Color.ORANGE
+            else:
+                color = Color.RED
 
-        draw_line(_ball.position, end_point, color, 6)
+            draw_line(_ball.position, end_point, color, 6)
 
 func _mouse_released():
     print_debug("Ball's linear velocity: %s" % _ball.get_linear_velocity().length())
