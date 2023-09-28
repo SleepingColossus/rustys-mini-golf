76fadf702f7dcb6ba1906e7a2b07eef1dbba7a9c
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-09-28 21:05:08 +0100

    Add back button to level select

--- Src/LevelSelect/LevelSelect.gd
+++ Src/LevelSelect/LevelSelect.gd
@@ -17,3 +17,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     pass
+
+
+func _on_back_button_pressed() -> void:
+    get_tree().change_scene_to_file("res://MainMenu/MainMenu.tscn")
