7447645a87b0a7856b09799cb4180cbe6da51500
Aleksandar Mancic <aca95mancic@gmail.com>
2023-09-30 17:57:53 +0100

    Change setting in Godot

--- Src/MainMenu/MainMenu.gd
+++ Src/MainMenu/MainMenu.gd
@@ -3,4 +3,4 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://LevelSelect/LevelSelect.tscn")
+    get_tree().change_scene_to_file("res://LevelSelect/LevelSelect.tscn")
